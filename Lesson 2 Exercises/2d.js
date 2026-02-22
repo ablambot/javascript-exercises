a4e2b851437f89f1ecb1dd30b20d12cbbc04d89d
Your Name <you@example.com>
2026-02-22 21:49:25 +0800

    update

---
+++ Lesson 2 Exercises/2d.js
@@ -0,0 +1,2 @@
+// Calculate a 10% tax for the total in exercise 2c.
+(18.50 + 2 * 7.50) * 0.1
---
+++ Lesson 2 Exercises/2e.js
@@ -0,0 +1,2 @@
+// Calculate a 20% tax for the total in 2c (remember that 1% = 1/100, so 20% = 20 / 100 = 0.2).
+(18.50 + 2 * 7.50) * 0.2
