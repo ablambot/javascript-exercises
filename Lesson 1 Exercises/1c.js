901deeba1d9dc750d9b62ef6672b039abd4116af
Your Name <you@example.com>
2026-02-22 21:43:47 +0800

    fix

---
+++ Lesson 1 Exercises/1c.js
@@ -0,0 +1,2 @@
+// Using math, calculate 10 + 5 in the Console.
+10 + 5
---
+++ Lesson 1 Exercises/1f.js
@@ -0,0 +1,2 @@
+// Use document.body.innerHTML = ...; to display 'Good morning!" on the web page.
+document.body.innerHTML = 'good morning'
